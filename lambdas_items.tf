4fc65c3be4b3ea44045c0449eb73fb1a55d201d2
David G Snyder <dsnyder74@gmail.com>
2025-06-26 16:10:26 -0400

    Added lambda for getting ingredients for a recipe and for setting a preferred recipe

--- lambdas_items.tf
+++ lambdas_items.tf
@@ -132,3 +132,57 @@ module "delete_item" {
   db_endpoint = module.rds.rds_endpoint
   db_port     = module.rds.rds_port
 }
+
+# Get Ingredients for Item - Lambda function to fetch an ingredient list for an Item.
+module "get_ingredients_for_item" {
+  source = "./modules/base_lambda_api"
+
+  function_name = "${var.environment_name}-pgGetIngredientsForItem"
+  route_key     = "GET /pgGetIngredientsForItem/{id}"
+
+  layers = local.common_layers
+
+  environment_name          = var.environment_name
+  lambda_execution_role_arn = module.lambda_security.lambda_security_role_arn
+  lambda_sg_id              = module.lambda_security.lambda_sg_id
+  lambda_api_id             = module.api_gateway.lambda_api_id
+  lambda_api_execution_arn  = module.api_gateway.lambda_api_execution_arn
+  lambda_api_dependency     = module.api_gateway.api_dependency
+  subnet_ids                = module.networking.private_subnet_ids
+  vpc_id                    = module.networking.vpc_id
+
+  additional_environment_variables = {
+    STAGE           = var.environment_name
+    ALLOWED_ORIGINS = var.allowed_origins
+  }
+
+  db_endpoint = module.rds.rds_endpoint
+  db_port     = module.rds.rds_port
+}
+
+# Set Preferred Recipe ID for Item - Lambda function to set the preferred recipe ID for an Item.
+module "set_preferred_recipe_for_item" {
+  source = "./modules/base_lambda_api"
+
+  function_name = "${var.environment_name}-pgSetPreferredRecipeForItem"
+  route_key     = "PATCH /pgSetPreferredRecipeForItem/{id}"
+
+  layers = local.common_layers
+
+  environment_name          = var.environment_name
+  lambda_execution_role_arn = module.lambda_security.lambda_security_role_arn
+  lambda_sg_id              = module.lambda_security.lambda_sg_id
+  lambda_api_id             = module.api_gateway.lambda_api_id
+  lambda_api_execution_arn  = module.api_gateway.lambda_api_execution_arn
+  lambda_api_dependency     = module.api_gateway.api_dependency
+  subnet_ids                = module.networking.private_subnet_ids
+  vpc_id                    = module.networking.vpc_id
+
+  additional_environment_variables = {
+    STAGE           = var.environment_name
+    ALLOWED_ORIGINS = var.allowed_origins
+  }
+
+  db_endpoint = module.rds.rds_endpoint
+  db_port     = module.rds.rds_port
+}
